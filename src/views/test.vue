eae807a9a65cd94cafb0cc34ae5b9e0bc9b831d3
chenzhenjin <971561770@qq.com>
2021-05-06 09:33:19 +0800

    fix(reactive): 只能处理对象

--- src/views/test.vue
+++ src/views/test.vue
@@ -4,7 +4,7 @@
   <button @click="add3">add3</button>
   <div>{{ num1 }}</div>
   <div>{{ num2 }}</div>
-  <div>{{ num3 }}</div>
+  <div>{{ num3obj.num3 }}</div>
 </template>
 
 <script lang="ts">
@@ -12,11 +12,11 @@ import { ref, reactive } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  setup() {
+  setup(props, context) {
     let num1 = 2
-    let num2 = ref(2)
-    let num3 = reactive(2)
-    console.log('num', num1, num2, num3)
+    let num2 = ref(2) //在setup内使用需要.value,给定值返回一个响应式数据对象
+    let num3obj = reactive({ num3: 3 })//接收一个普通对象，返回一个响应式的数据对象
+    console.log('num', num1, num2, num3obj)
     function add1() {
       num1++
     }
@@ -24,12 +24,12 @@ export default {
       num2.value++
     }
     function add3() {
-      num3++
+      num3obj.num3++
     }
     return {
       num1,
       num2,
-      num3,
+      num3obj,
       add1,
       add2,
       add3
